c9df17bd2e7c32ebea88d7192956e3ddaf283d12
maauso <m.auso.p@gmail.com>
2016-12-29 17:12:33 +0100

    Auto stash for cherrypick of "Debug mode on"
    Test mode

--- consul-template-start.sh
+++ consul-template-start.sh
@@ -1,2 +1,2 @@
-/consul-template -log-level=debug -dedup -exec-splay=5s -wait=10s:11s -reload-signal="SIGHUP" -template "haproxy.cfg.ctmpl:haproxy.cfg" -exec "./run.sh" -consul ${CONSUL_SERVER}
+/consul-template -log-level=warn -dedup -exec-splay=5s -wait=10s:11s -reload-signal="SIGHUP" -template "haproxy.cfg.ctmpl:haproxy.cfg" -exec "./run.sh" -consul ${CONSUL_SERVER}
 #${CONSUL_SERVER}
