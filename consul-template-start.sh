c50b4ae531b632b326d559c7a799b3fad1f71087
maauso <m.auso.p@gmail.com>
2017-01-10 15:19:26 +0100

    add max-stale option and comments.

--- consul-template-start.sh
+++ consul-template-start.sh
@@ -1,2 +1,15 @@
-/consul-template -log-level=warn -dedup -exec-splay=5s -wait=10s:11s -reload-signal="SIGHUP" -template "haproxy.cfg.ctmpl:haproxy.cfg" -exec "./run.sh" -consul ${CONSUL_SERVER}
-#${CONSUL_SERVER}
+/consul-template -log-level=warn -max-stale=10m -dedup -exec-splay=5s -wait=10s:11s -reload-signal="SIGHUP" -template "haproxy.cfg.ctmpl:haproxy.cfg" -exec "./run.sh" -consul ${CONSUL_SERVER}
+
+
+#max-stale
+#This is the maximum interval to allow "stale" data. By default, only the Consul leader will respond to queries; so this option allows any follower to respond to a query
+
+#dedup 
+#Consul Template uses leader election on a per-template basis to have only a single node perform the queries. Results are shared among other instances rendering the same template by passing compressed data through the Consul K/V store.
+
+#exec-splay
+#This is a random splay to wait before killing the command, splay value to prevent all child processes from reloading at the same time
+
+#wait
+#This is the `minimum(:maximum)` to wait before rendering a new template
+
